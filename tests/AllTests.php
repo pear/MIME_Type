d6718b5d2b4b1a7f5b5eb4ed764c15ad0b0cb248
Daniel O'Connor <clockwerx@php.net>
2010-02-12 02:47:03 +0000

    Fixed AllTests.php
    git-svn-id: http://svn.php.net/repository/pear/packages/MIME_Type/trunk@294963 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -3,7 +3,6 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
     define('PHPUnit_MAIN_METHOD', 'MIME_Type_AllTests::main');
 }
 
-require_once 'PHPUnit/Framework/TestSuite.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
 
 chdir(dirname(__FILE__) . '/../');
